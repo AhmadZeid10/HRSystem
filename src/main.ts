47d1e37b071ed828daa9e4cfaad04d909ac03f2b
AhmadZeid10 <ahmad_Zad10@hotmail.com>
2017-08-03 16:29:08 +0300

    disable  Producation mode

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 
-enableProdMode();
+//enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
 
